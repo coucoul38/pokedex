9f03a0328fb5ffa74c357ac127f3ee6f5576d767
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-05 11:29:03 +0100

    amogus

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dbo = require("./db/db");
 const app = express();
 const port = 4444;
 const bodyParser = require('body-parser');
+const db = require("./db/db");
 
 dbo.connectToServer();
 
@@ -18,7 +19,17 @@ app.listen(port, function () {
 
 app.post('/pokedex/insert', (req, res) => {
   const body = req.body;
-  console.log('Got body:', body);
+  console.log('Got body: ', body);
+
+  const name = req.query.name;
+  const no = req.query.no;
+  const img = req.query.img;
+  console.log('name: '+name);
+  console.log('no: '+no);
+  console.log('img: '+img);
+
+  db.pokemons.insert({ name: name, no: no, img: img });
+  console.log("Inserted into db");
   //on code ensuite l'insertion dans mongoDB, lisez la doc hehe !!
   res.json(body);
 });
