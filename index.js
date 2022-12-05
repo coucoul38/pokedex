0f64c65ae8a3633f9f93d030f5f9b990266e88ef
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-05 09:58:11 +0100

    amongus

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 4444;
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`App listening on port ${port}!`);
+});
