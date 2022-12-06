e5cfe3046d86c85a29ae50841a7e402e0122efa9
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-06 08:36:00 +0100

    er

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const dbo = require("./db/db");
 const app = express();
 const port = 4444;
 const bodyParser = require('body-parser');
+const db = require("./db/db");
 
 dbo.connectToServer();
+
+//var jsonParser = bodyParser.json();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,7 +20,6 @@ app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
 
-//----------Add a pokemon to the Pokedex----------
 app.post('/pokedex/insert', (req, res) => {
   const body = req.body;
   console.log('Got body: ', body);
@@ -24,35 +27,15 @@ app.post('/pokedex/insert', (req, res) => {
   const name = req.query.name;
   const no = req.query.no;
   const img = req.query.img;
+  console.log('name: '+name);
+  console.log('no: '+no);
+  console.log('img: '+img);
 
-  let input = {"name": name, "no": no, "img": img};
-  console.log('Object to send: ',input);
-
-  const dbConnect = dbo.getDb();
-  dbConnect
-    .collection("pokemons")
-    .insertOne(input);
-  res.json(req.body.name)
+  db.pokemons.insert({ name: name, no: no, img: img });
+  console.log("Inserted into db");
+  //on code ensuite l'insertion dans mongoDB, lisez la doc hehe !!
+  res.json(body);
 });
-
-//----------Delete a pokemon from the Pokedex----------
-
-app.post('/pokedex/delete', (req, res) => {
-  const body = req.body;
-  console.log('Got body: ', body);
-  
-  const no = req.query.no;
-
-  let input = {"name": name, "no": no, "img": img};
-  console.log('Object to send: ',input);
-
-  const dbConnect = dbo.getDb();
-  dbConnect
-    .collection("pokemons")
-    .insertOne(input);
-  res.json(req.body.name)
-});
-
 
 //Get pokemons from db
 app.get("/pokedex/pokemons", function (req, res) {
@@ -71,3 +54,4 @@ app.get("/pokedex/pokemons", function (req, res) {
       }
     });
 });
+
