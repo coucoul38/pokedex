54a030b54ebbef09fcb5c478a66971b9ea1cda20
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-07 12:59:13 +0100

    Co-authored-by: Loris Fiard <Glocax@users.noreply.github.com>

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+            <Switch>
+              <Route exact path="/"> //ici on met l'URL dans le navigateur
+                <Home /> //ici on donne la page à afficher en fonction de cette URL
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/dashboard">
+                <Dashboard />
+              </Route>
+            </Switch>
+        </Router>
   );
 }
 
---
+++ frontend/myapp/src/pages/page.js
@@ -0,0 +1,5 @@
+function Home(props){
+    return <h1>Acceuil</h1>;
+}
+
+export default Home;
