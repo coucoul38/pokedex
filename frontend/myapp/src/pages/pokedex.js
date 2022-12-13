71cb703a6b81eb0a087137b12927b849f77c90a4
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-12-13 12:16:18 +0100

    images not working

--- frontend/myapp/src/pages/pokedex.js
+++ frontend/myapp/src/pages/pokedex.js
@@ -35,9 +35,9 @@ function Pokedex(){
             return (<div key={key} className="bloc-pokemon pixel">
               <img className='avatar' src={'https://img.pokemondb.net/sprites/sword-shield/icon/'+pokemon.name.toLowerCase()+'.png'} /*{pokemon.img}*/ alt="Pokemon" />
               <h2>{pokemon.name}</h2>
-              No{pokemon.no}
-              <img className='type' src={'../img/'+pokemon.type1+'.png'}></img>
-              <img className='type' src={'/src/img/'+pokemon.type2+'.png'}></img>
+              No:{pokemon.no}
+              <img className='type' src={'img/'+pokemon.type1+'.png'} alt='Type1'></img>
+              <img className='type' src={'img/'+pokemon.type2+'.png'} alt='Type2'></img>
               {<button onClick={()=>addToPokedex(pokemon.no)} className="poke-button"><img src="https://img.pokemondb.net/sprites/items/master-ball.png" alt="Capturer !"className='balls'/></button>}
             </div>)
           })
